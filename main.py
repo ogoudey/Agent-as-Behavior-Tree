415ebef664843426f9819ae7f7ac7f2185c2032c
Olin <olin.goog@gmail.com>
2025-11-23 10:45:32 -0500

    random chngee

--- main.py
+++ main.py
@@ -6,26 +6,32 @@
 #
 
 import bt_library as btl
-
+import time
 from bt.behavior import robot_behavior
 from collections import defaultdict
 
+def main():
 # Main body of the assignment
-current_blackboard = btl.Blackboard()
+    current_blackboard = btl.Blackboard()
 
-# You must determine what to store in the blackboard. This is just an example.
+    # You must determine what to store in the blackboard. This is just an example.
 
-# You must determine how your solution terminates. This is just an example.
-done = 1000
-import time
+    # You must determine how your solution terminates. This is just an example.
+    done = 1000
 
-current_blackboard.set_in_environment('DONE', False)
 
-while not current_blackboard.get_in_environment('DONE', False):
-        btl.print_blackboard('BLACKBOARD BEFORE', current_blackboard)
-        btl.print_header('TREE EVALUATION')
-        result = robot_behavior.evaluate(current_blackboard)
-        btl.print_footer()
-        # Print the state of the tree nodes before the evaluation
-        btl.print_blackboard('BLACKBOARD AFTER', current_blackboard)
+    current_blackboard.set_in_environment('DONE', False)
+
+    while not current_blackboard.get_in_environment('DONE', False):
+            btl.print_blackboard('BLACKBOARD BEFORE', current_blackboard)
+            btl.print_header('TREE EVALUATION')
+            result = robot_behavior.evaluate(current_blackboard)
+            btl.print_footer()
+            # Print the state of the tree nodes before the evaluation
+            btl.print_blackboard('BLACKBOARD AFTER', current_blackboard)
+            
+if __name__ == "__main__":
+    main()
+
+
 
